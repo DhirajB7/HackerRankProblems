c2037c8d88e2a6192b2ac572c0312f0e082fd172
dhirajb7 <bond.dhiraj@gmail.com>
2020-06-23 18:15:01 +0530

    new changes

--- EqualizeTheArray.java
+++ EqualizeTheArray.java
@@ -1,13 +1,46 @@
 package hackerrank;
 
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class EqualizeTheArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		for (int i = 0; 0; i++) {
-			System.out.println("hi");
+
+		Scanner scan = new Scanner(System.in);
+
+		TreeMap<Integer, Integer> tMap = new TreeMap<Integer, Integer>();
+		TreeMap<Integer, Integer> answerMap = new TreeMap<Integer, Integer>();
+
+		int lc = scan.nextInt();
+
+		for (int i = 0; i < lc; i++) {
+
+			int key = scan.nextInt();
+
+			int value = 1;
+
+			if (tMap.containsKey(key)) {
+				value = tMap.get(key) + 1;
+			}
+
+			tMap.put(key, value);
 		}
+
+		scan.close();
+
+		for (Entry<Integer, Integer> a : tMap.entrySet()) {
+
+			int value = a.getKey();
+
+			int key = a.getValue();
+
+			answerMap.put(key, value);
+
+		}
+
+		System.out.println(lc - answerMap.lastKey());
 
 	}
 
